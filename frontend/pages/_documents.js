0c9205d5682dc37aa6852b0e275cde328737e1e1
JB-Dev <jibaoke@gmail.com>
2022-09-19 12:52:54 +0200

    init project

---
+++ frontend/pages/_documents.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import CreateEmotionServer from '@emotion/server/create-instance';
+import createCache from '@emotion/cache';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="fr">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Body>
+          <Main />
+          <NextScript />
+        </Body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+  const cache = createCache({ key: 'css' });
+  const { extractCriticalToChunks } = CreateEmotionServer(cache);
+  ctx.renderPage = () =>
+    originalRenderPage({
+      //eslint-disable-next-line react/display-name
+      enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
+    });
+  const InitialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(InitialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join('')}`}
+      key={style.key}
+      //eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+  return {
+    ...InitialProps,
+    styles: [
+      ...React.Children.toArray(InitialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
