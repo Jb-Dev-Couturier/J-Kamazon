4d58f31018fc2941cdccb07364daad673ce8aba1
JB-Dev <jibaoke@gmail.com>
2022-09-26 21:37:02 +0200

    Update client.js

--- utils/client.js
+++ utils/client.js
@@ -4,7 +4,7 @@ import config from './config'
 const client = sanityClient({
     projectId:config.projectId,
     dataset:config.dataset,
-    useCdn: true,
+    useCdn: false,
     apiVersion:'2022-09-20'
 })
 
